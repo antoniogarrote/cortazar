40b01d3e13a55a920a89a26ae52f847de22b0f56
Antonio Garrote <antoniogarrote@gmail.com>
2016-03-24 21:54:14 +0000

    Parsing FSM specifications

--- src/fsm.rs
+++ src/fsm.rs
@@ -1,15 +1,27 @@
 use std::collections::HashMap;
+use std::fmt;
 
 pub struct FSM {
-    initial_state: String,
-    transitions: HashMap<String,HashMap<String,String>>
-}
-
-pub fn new(initial_state:String) -> FSM {
-    FSM { initial_state: initial_state, transitions: HashMap::new() }
+    pub initial_state: String,
+    pub transitions: HashMap<String,HashMap<String,String>>
 }
 
 impl FSM {
+
+    pub fn new(initial_state:String) -> FSM {
+        FSM { initial_state: initial_state, transitions: HashMap::new() }
+    }
+
+    pub fn build(initial_state:String, transitions_list:& [(String,String,String)]) -> FSM {
+        let mut fsm = FSM::new(initial_state);
+        for transition in transitions_list {
+            match transition.clone() {
+                (from,by,to) => fsm.add_transition(from,to,by)
+            }
+        }
+        fsm
+    }
+
     pub fn add_transition(&mut self, from:String, to:String, by:String) {
         if self.transitions.get(&from).is_some() {
             self.transitions.get_mut(&from).unwrap().insert(by, to);
@@ -18,5 +30,19 @@ impl FSM {
             new_transitions.insert(by, to);
             self.transitions.insert(from, new_transitions);
         }
+    }
+}
+
+impl fmt::Display for FSM {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let _ = write!(f,"\nFSM[\n - Initial state: {}\n - Transitions:",self.initial_state);
+        for from in self.transitions.keys() {
+            for by in self.transitions.get(from).unwrap().keys() {
+                let to = self.transitions.get(from).unwrap().get(by).unwrap();
+                let _ = write!(f, "\n   * {} -{}-> {}", from, by, to);
+            }
+        }
+        write!(f,"\n]\n")
     }
 }
--- src/specification.rs
+++ src/specification.rs
@@ -6,6 +6,8 @@ use std::io::prelude::*;
 use fsm::FSM;
 
 use yaml_rust::YamlLoader;
+use yaml_rust::yaml::Yaml;
+
 
 pub fn load(path:&String) {
     let path = Path::new(path);
@@ -27,7 +29,33 @@ pub fn load(path:&String) {
     };
 
     let loaded = YamlLoader::load_from_str(&content).unwrap();
-    println!("READ {:?}", loaded);
+    if loaded.len() == 1 {
+        let description = loaded[0].as_hash().unwrap();
+        let initial_state = description.get(&Yaml::String(String::from("initial-state")));
+        let fsm = match initial_state {
+            Some(&Yaml::String(_)) => {
+                let transitions = description.get(&Yaml::String(String::from("transitions")));
+                match transitions {
+                    Some(transitions) => {
+                        let initial_state_str = String::from(initial_state.unwrap().as_str().unwrap());
+                        let mut fsm = FSM::new(initial_state_str);
 
+                        for transition in transitions.as_vec().unwrap() {
+                            let transition_triple =  transition.as_vec().unwrap();
+                            let from = String::from(transition_triple[0].as_str().unwrap());
+                            let by =  String::from(transition_triple[1].as_str().unwrap());
+                            let to = String::from(transition_triple[2].as_str().unwrap());
+                            fsm.add_transition(from,to,by);
+                        }
+                        fsm
+                    },
+                    None => panic!("Missing transitions in FSM specification")
+                }
+            },
+            Some(v) => panic!("Unexpected input value for initial state {:?}",v),
+            None => panic!("Missing initial state in FSM specification")
+        };
 
+        println!("FSM READ!!! {}",&fsm);
+    }
 }
